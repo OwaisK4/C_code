81e1ef2ff703c042b6849b7e3d4e79dde6f32df4
Owais Ali Khan <owais@OwaisOS.owais>
2020-07-04 09:54:13 +0000

    Initial commit. Created git_repo for bits of C_code copied from Jon;s Erickson.

---
+++ byteSize.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	unsigned char uChar = 0xFF;
+	char Char = 0xFA;
+	unsigned short uShort = 0x4123;
+	short Short = 0x4444;
+	unsigned int uInt = 0xDEADBEEF;
+	int Int = 0xDEADBEEF;
+	unsigned long long uLong = 0xEFAD896896;
+	long long Long = 0xEEAD8897689;
+	float Float = 3.142;
+	return 0;
+}
+
